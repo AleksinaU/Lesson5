b266d750eabae3e0090cf5af5c08fa594c3bac52
Юлия Алексина <ulchez@mail.ru>
2022-07-18 21:30:21 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите семь чисел");
+            int[] array = new int[7];
+            float s = 0;
+            int k = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                s += array[i];
+                k++;
+            }
+            Console.WriteLine("Среднее арифметическое равно {0:f2}", s / k) ;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int x = 0;
+            int[,] array= new int [n,n];
+            for (int i = 0; i < n;  i++)             
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    x++;
+                    if (x % 2 == 0) Console.Write("0");
+                    else Console.Write("1");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
